764626455d68cdb4ab238732c45853f3e1c04b44
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-01-16 17:00:20 +0600

    added spacer to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     return (
         <div className="pt-20">
             <Banner></Banner>
+             <div className="min-h-[75vh] md:min-h-[80vh] lg:min-h-[70vh] w-full"></div>
             <PartnersSection></PartnersSection>
         </div>
     );
