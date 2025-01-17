d79e5a4e7c1ee6b107e63512bc0c245294be9675
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-01-18 00:27:04 +0600

    daynamic My Prifile page done

--- src/pages/DashboardPages/Admin/MyProfile.jsx
+++ src/pages/DashboardPages/Admin/MyProfile.jsx
@@ -1,36 +1,42 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/AxiosSecure/useAxiosSecure";
+import useAuth from "../../../hooks/GetAuthInfo/useAuth";
 
 const MyProfile = () => {
-  const user = {
-    name: "John Doe",
-    role: "Admin",
-    image: "https://via.placeholder.com/150",
-    email: "john.doe@example.com",
-    phone: "+1234567890",
-  };
+  
+  const {user} = useAuth();
+  const axiosSecure = useAxiosSecure();
 
+  const {data: myProfileData = {}, } = useQuery({
+    queryKey: ["user-profile", user?.email], 
+    enabled: !!user?.email,
+    queryFn: async ()=> {
+      const {data} = await axiosSecure.get(`/user-profile/${user?.email}`);
+      return data;
+    }
+  })
+
+
+console.log(myProfileData);
   return (
     <div className="p-6 bg-white dark:bg-dark-background rounded-lg shadow-md max-w-md mx-auto">
       <h2 className="text-2xl font-bold mb-6">My Profile</h2>
       <div className="flex items-center space-x-6">
         <img
-          src={user.image}
-          alt={user.name}
+          src={myProfileData.photo}
+          alt={myProfileData.name}
           className="w-24 h-24 rounded-full object-cover"
         />
         <div>
-          <h3 className="text-xl font-semibold">{user.name}</h3>
-          <p className="text-sm text-gray-500">{user.role}</p>
+          <h3 className="text-xl font-semibold">{myProfileData.name}</h3>
+          <p className="text-sm text-gray-500">{myProfileData.role}</p>
         </div>
       </div>
 
       <div className="mt-6 space-y-4">
         <div className="flex items-center space-x-4">
           <strong className="text-gray-700">Email:</strong>
-          <p>{user.email}</p>
-        </div>
-        <div className="flex items-center space-x-4">
-          <strong className="text-gray-700">Phone:</strong>
-          <p>{user.phone}</p>
+          <p>{myProfileData.email}</p>
         </div>
       </div>
     </div>
