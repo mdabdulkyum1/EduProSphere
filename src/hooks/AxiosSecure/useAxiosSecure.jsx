bb65ceafc5882e96226b9242a3ef388b888c5470
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-01-20 18:54:55 +0600

    ready to deploy

--- src/hooks/AxiosSecure/useAxiosSecure.jsx
+++ src/hooks/AxiosSecure/useAxiosSecure.jsx
@@ -4,8 +4,8 @@ import { useNavigate } from 'react-router-dom';
 
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000'
-    // baseURL: 'https://edu-pro-sphere.vercel.app'
+    // baseURL: 'http://localhost:5000'
+    baseURL: 'https://edu-pro-sphere.vercel.app'
   });
 
   
